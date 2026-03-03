0fc6e41b64aec702050683fafae4252335ededd5
Voodoo30fps <voodoo30fps@gmail.com>
2026-03-03 19:18:46 +0200

    useImperativeHandle hook

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,12 +1,23 @@
-import { forwardRef } from 'react';
-// ^ for older versions of React, you would import forwardRef from 'react' like this:
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+// ^ forwardRef for older versions of React, you would import forwardRef from 'react' like this
+// ^ useImperativeHandle allows you to customize the instance value that is exposed to parent components when using ref. In this case, it allows the parent component to call the open method on the ResultModal component.
 
 const ResultModal = forwardRef(function ResultModal(
   { result, targetTime },
   ref,
 ) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
-    <dialog ref={ref} className='result-modal'>
+    <dialog ref={dialog} className='result-modal'>
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
